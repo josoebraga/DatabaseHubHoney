4245d9277a4affb3f8b0e76af7321d3494ae6bf6
Josoé Schmidt Braga <42846760+josoebraga@users.noreply.github.com>
2023-05-08 23:24:03 -0300

    Dashboard 08/05/2023 1n

--- app/Http/Livewire/Relatorios/VisaoMailingComponent.php
+++ app/Http/Livewire/Relatorios/VisaoMailingComponent.php
@@ -18,18 +18,43 @@ class VisaoMailingComponent extends Component
     public $tabelaSelecionada;
     public $data;
     private $historicos;
+    private $contagens;
+    private $total;
 
     public function carregar()
     {
         $carbon = Carbon::parse($this->data);
         $dataFormatada = $carbon->format('Y-m-d');
+        $colunas = [];
+        $colunasTmp = [];
         $this->historicos = Modificacoes::leftJoin('users', 'users.id', '=', 'modificacoes.user_id')->where('nome_tabela', '=', "$this->tabelaSelecionada")->whereRaw("to_char(modificacoes.created_at, 'YYYY-MM-DD') = '$dataFormatada'")->select('modificacoes.*', 'users.name')->orderBy('id', 'asc')->paginate(100);
         #dd($this->historicos);
-        #foreach($this->historicos as $key => $historico) {
-        #    dd(json_decode($historico->historico));
-        #}
 
+        foreach($this->historicos as $key => $historico) {
+            #dd(json_decode($historico->historico));
+            $json = json_decode($historico->historico);
+            if (isset($json) && property_exists($json, 'invalidos')) {
 
+                foreach ($json->invalidos as $key => $value) {
+                        array_push($colunas, [$key => 0]);
+                }
+            }
+        }
+
+        $resultado = array_reduce($colunas, function($acumulado, $atual) {
+            foreach ($atual as $chave => $valor) {
+                if (!isset($acumulado[$chave])) {
+                    $acumulado[$chave] = [];
+                }
+                $acumulado[$chave][] = $valor;
+            }
+            return $acumulado;
+        }, []);
+
+        $this->contagens = array_map(function($valor) {
+            return count($valor);
+        }, $resultado);
+        $this->total = array_sum($this->contagens);
     }
 
     public function mount()
